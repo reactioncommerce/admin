1896ef08d33e7d8d618d0179239bb0f0965c3e05
Mohan Narayana <mohan.narayana@mailchimp.com>
2021-03-09 16:46:55 -0600

    chore:adding eslint
    Signed-off-by: Mohan Narayana <mohan.narayana@mailchimp.com>

--- tests/testSetup.test.js
+++ tests/testSetup.test.js
@@ -1,7 +1,7 @@
-//This test file is added to run CI tests
-//It should be deleted once we have legit tests
-const mockCallback = jest.fn(x => 2 * x);
+// This test file is added to run CI tests
+// It should be deleted once we have legit tests
+const mockCallback = jest.fn((val) => 2 * val);
 
 test("expect to return a value", () => {
-    expect(mockCallback(4)).toBe(8);
+  expect(mockCallback(4)).toBe(8);
 });
