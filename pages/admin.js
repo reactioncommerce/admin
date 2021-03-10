d14e41e76215961e8a6e292fbea7c33faee9014e
jrw421 <jessica.wolvington@gmail.com>
2021-03-10 10:59:17 -0500

    chore: linting fixes
    Signed-off-by: jrw421 <jessica.wolvington@gmail.com>

--- pages/admin.js
+++ pages/admin.js
@@ -1,8 +1,8 @@
 import * as React from "react";
-import { Admin } from 'react-admin';
-import jsonServerProvider from 'ra-data-json-server';
+import { Admin } from "react-admin";
+import jsonServerProvider from "ra-data-json-server";
 
-const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com');
+const dataProvider = jsonServerProvider("https://jsonplaceholder.typicode.com");
 const AdminHome = () => <Admin dataProvider={dataProvider} />;
 
 export default AdminHome;
