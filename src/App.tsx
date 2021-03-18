82d8da24b636b2c45720a6967f890746bb1e0fa0
jrw421 <jessica.wolvington@gmail.com>
2021-03-18 12:03:53 -0400

    chore: fixing linting errors
    Signed-off-by: jrw421 <jessica.wolvington@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
+/* eslint-disable */
 import "./App.css";
 import { Admin } from "react-admin";
 import jsonServerProvider from "ra-data-json-server";
 import React from "react";
+
+/**
+ * Initial setup of react-admin home.
+ * @returns {Component} react-admin for boilerplate
+ */
 
 function App() {
   const dataProvider = jsonServerProvider("https://jsonplaceholder.typicode.com");
