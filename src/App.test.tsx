441a3e21f6f3632cafcaa230ed05ff9cb3ce5587
Mohan Narayana <mohan.narayana@mailchimp.com>
2021-03-18 16:30:31 -0500

    chore: added lint for typescript
    Signed-off-by: Mohan Narayana <mohan.narayana@mailchimp.com>

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
-import { render, screen, waitFor } from '@testing-library/react';
-import { act } from 'react-dom/test-utils';
-import App from './App';
+import React from "react";
+import { render, screen, waitFor } from "@testing-library/react";
+import { act } from "react-dom/test-utils";
+import App from "./App";
 
-test('Renders react admin', async () => {
+test("Renders react admin", async () => {
   act(() => {
     render(<App />);
   });
