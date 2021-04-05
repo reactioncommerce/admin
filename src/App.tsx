4a0c522027c59b895ee2caccd52878d1e3f42689
Mohan Narayana <mohan.narayana@mailchimp.com>
2021-04-05 12:41:48 -0500

    chore: added authentication component
    Signed-off-by: Mohan Narayana <mohan.narayana@mailchimp.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,38 @@
 /* eslint-disable */
+import PropTypes from "prop-types";
+import {AuthenticationProvider, OidcSecure} from "@axa-fr/react-oidc-context";
 import "./App.css";
 import { Admin } from "react-admin";
+import { getOidcProps } from "./lib/authentication";
 import jsonServerProvider from "ra-data-json-server";
-import React from "react";
 
 /**
  * Initial setup of react-admin home.
  * @returns {Component} react-admin for boilerplate
  */
 
-function App() {
+function App(){
+  
+  // Create OIDC props to be used on the AuthenticationProvider
+  const authenticationProviderProps = getOidcProps(
+    "open-commerce-admin",
+    "http://localhost:4444",
+    "http://localhost:4080"
+  );
   const dataProvider = jsonServerProvider("https://jsonplaceholder.typicode.com");
   return (
-    <div className="App">
-      <Admin dataProvider={dataProvider} />
+    <div id="app">
+      <AuthenticationProvider {...authenticationProviderProps}>
+        {/* <OidcSecure> */}
+          <Admin dataProvider={dataProvider} />
+        {/* </OidcSecure> */}
+      </AuthenticationProvider>
     </div>
   );
 }
+
+App.propTypes = {
+  authenticationProviderProps: PropTypes.object,
+};
 
 export default App;
