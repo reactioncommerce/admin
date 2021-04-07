0339bbaecfa7d6a45042659b854193cc78c17054
Mohan Narayana <mohan.narayana@mailchimp.com>
2021-04-07 15:23:05 -0500

    chore: move tests directory to src
    Signed-off-by: Mohan Narayana <mohan.narayana@mailchimp.com>

--- src/tests/testSetup.test.js
+++ src/tests/testSetup.test.js
@@ -1,7 +1,7 @@
 // This test file is added to run CI tests
 // It should be deleted once we have legit tests
 const mockCallback = jest.fn((val) => 2 * val);
-
+const value = mockCallback(4);
 test("expect to return a value", () => {
-  expect(mockCallback(4)).toBe(8);
+  expect(value).toBe(8);
 });
